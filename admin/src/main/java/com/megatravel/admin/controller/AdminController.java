03254dd618e8dfe7074da72028c51c2ba73e177c
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-06-17 22:42:52 +0200

    Implemented services for contoller

--- admin/src/main/java/com/megatravel/admin/controller/AdminController.java
+++ admin/src/main/java/com/megatravel/admin/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.megatravel.admin.controller;
 
+import com.megatravel.admin.dto.AgentDTO;
+import com.megatravel.admin.dto.SmestajDTO;
+import com.megatravel.admin.dto.UslugaDTO;
 import com.megatravel.admin.model.*;
 import com.megatravel.admin.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 
 import java.util.List;
 
@@ -14,19 +20,26 @@ public class AdminController {
     @Autowired
     AdminService adminService;
 
+
     @RequestMapping(value = "/services", method = RequestMethod.POST)
-    public Usluga addUsluga(@RequestBody Usluga usluga){
+    public Usluga addUsluga(@RequestBody UslugaDTO usluga){
         return adminService.addUsluga(usluga);
     }
 
     @RequestMapping(value = "/services/{id}", method = RequestMethod.DELETE)
-    public Usluga deleteUsluga(@PathVariable("id")Long id){
-        return adminService.deleteUsluga(id);
+    public ResponseEntity<String> deleteUsluga(@PathVariable("id")Long id){
+        try {
+            String message = adminService.deleteUsluga(id);
+            return new ResponseEntity<String>(message, HttpStatus.OK);
+        }catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @RequestMapping(value = "/services/{id}", method = RequestMethod.PUT)
-    public Usluga updateUsluga(@RequestBody Usluga usluga){
-        return adminService.updateUsluga(usluga);
+    public Usluga updateUsluga(@RequestBody UslugaDTO usluga, @PathVariable("id") Long id){
+        return adminService.updateUsluga(usluga, id);
     }
 
     @RequestMapping(value = "/services", method = RequestMethod.GET)
@@ -39,28 +52,39 @@ public class AdminController {
         return adminService.setTip(smestaj.getId(), smestaj.getTipSmestaja());
     }
 
-    @RequestMapping(value = "/comments", method = RequestMethod.PUT)
+    @RequestMapping(value="/accomodation", method = RequestMethod.POST)
+    public Smestaj addAccomodation(@RequestBody SmestajDTO smestaj)
+    {
+        return adminService.addAccomodation(smestaj);
+    }
+
+    @RequestMapping(value = "/comments", method = RequestMethod.GET)
+    public List<Komentar> getComments()
+    {
+        return adminService.getUnapprovedComments();
+    }
+    @RequestMapping(value = "/comments/{id}", method = RequestMethod.PUT)
     public Komentar odobriKomentar(@PathVariable("id")Long id){
         return adminService.odobriKomentar(id);
     }
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
-    public TKorisnik blockKorisnik(@PathVariable("id")Long id){
+    public User blockKorisnik(@PathVariable("id")Long id){
         return adminService.blockKorisnik(id);
     }
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.POST)
-    public TKorisnik activatKorisnik(@PathVariable("id")Long id){
-        return adminService.activatKorisnik(id);
+    public User activatKorisnik(@PathVariable("id")Long id){
+        return adminService.activateKorisnik(id);
     }
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    public TKorisnik deleteKorisnik(@PathVariable("id")Long id){
+    public User deleteKorisnik(@PathVariable("id")Long id){
         return adminService.deleteKorisnik(id);
     }
 
     @RequestMapping(value = "/agent", method = RequestMethod.POST)
-    public TKorisnik addAgent(@RequestBody TAgent agent){
+    public User addAgent(@RequestBody AgentDTO agent){
         return adminService.addAgent(agent);
     }
 }
