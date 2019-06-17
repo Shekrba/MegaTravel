8afc0858e49f9484af769a78a12c4ec639385619
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-06-17 22:44:58 +0200

    test data

---
+++ admin/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+insert into usluga (id, naziv, cena, opis) values (1,'spa', 20, 'Topconma');
+insert into usluga (id, naziv, cena, opis) values (2,'spazz', 20, 'Topconma');
+insert into usluga (id, naziv, cena, opis) values (3,'spacx', 20, 'Topconma');
+insert into usluga (id, naziv, cena, opis) values (4,'spaqew', 20, 'Topconma');
+
+insert into user (ime, prezime, email, role , status) values  ('Nikola', 'Mandic', 'mandaaaa96@yahoo.com', 'ADMIN', 'PENDING');
+insert into user (ime, prezime, email, role , status) values  ('Petar', 'Mandic', 'mandaaaa96@yahoo.com', 'USER', 'PENDING');
+insert into user (ime, prezime, email, role , status) values  ('Simo', 'Mandic', 'mandaaaa96@yahoo.com', 'AGENT', 'PENDING');
+insert into user (ime, prezime, email, role , status) values  ('DJURO', 'Mandic', 'mandaaaa96@yahoo.com', 'USER', 'PENDING');
+insert into user (ime, prezime, email, role , status) values  ('TOMKET', 'Mandic', 'mandaaaa96@yahoo.com', 'AGENT', 'PENDING');
+insert into user (ime, prezime, email, role , status) values  ('AAAA', 'Mandic', 'mandaaaa96@yahoo.com', 'AGENT', 'ACTIVE');
+
+
+insert into komentar(odobren, tekst, korisnik_id ) values ( false, 'Mars',1);
+insert into komentar(odobren, tekst, korisnik_id ) values ( false, 'Mdasdasars',1);
+insert into komentar(odobren, tekst, korisnik_id ) values ( false, 'asd',1);
+insert into komentar(odobren, tekst, korisnik_id ) values ( true, 'Msadars',1);
+insert into komentar(odobren, tekst, korisnik_id ) values ( true, 'Mdasars',2);
+insert into komentar(odobren, tekst, korisnik_id ) values ( false, 'Maasrs',2);
