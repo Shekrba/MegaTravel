800867693c1b2824d1b3b0693354e07e13616bfa
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-06-20 22:00:48 +0200

    small changes

--- agent-service/src/main/java/com/megatravel/agentservice/controller/AgentController.java
+++ agent-service/src/main/java/com/megatravel/agentservice/controller/AgentController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "api", produces = "application/json;charset=UTF-8")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class AgentController {
 
     @Autowired
