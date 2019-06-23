6362c883ff756a79bf3b58827291da49979212d7
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-06-23 21:24:55 +0200

    data changes

--- agent-back/src/main/resources/data.sql
+++ agent-back/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-/*insert into usluga (id, naziv, cena, opis) values (1,'Spa', 20, 'For bath.');
+insert into usluga (id, naziv, cena, opis) values (1,'Spa', 20, 'For bath.');
 insert into usluga (id, naziv, cena, opis) values (2,'WIFI', 5, 'Internet.');
 insert into usluga (id, naziv, cena, opis) values (3,'Breakfast', 10, 'For eat.');
 insert into usluga (id, naziv, cena, opis) values (4,'Parking', 15, 'Parking');
@@ -99,7 +99,7 @@ INSERT INTO poruka(id, datum_slanja, posaljilac, primalac, sadrzaj, status) VALU
 
 
 
-*/
+
 
 
 
